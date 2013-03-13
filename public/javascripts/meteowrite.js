178dc4414b1fa4e09314acbaa8a2c148a862da34
Ubuntu <ubuntu@ip-10-245-79-26.ec2.internal>
2013-03-13 19:12:30 +0000

    pulling body from article and sending it server

--- public/javascripts/meteowrite.js
+++ public/javascripts/meteowrite.js
@@ -1,17 +1,22 @@
 
 $(document).ready(function(){
-    //var url = "http://ec2-54-235-227-153.compute-1.amazonaws.com:5000/articleParser";
-    var url = "/articleParser";
-    var data = new Object();
-    var body = $("#articleBody");
-    
+  console.log("where you at");
+  var url = "http://ec2-54-235-227-153.compute-1.amazonaws.com:5000/articleParser";
+    //var url = "/articleParser";
+    var data = {};
+    var body = $("#articlebody").text();
+    var n=body.indexOf("© Copyright The Sacramento Bee.  All rights reserved.");
+    body = body.substring(0,n);
+    body = body.trim();
     console.log(body);
-    data["body"] = body;
+    
+    data.body = body;
 
     $.ajax({
 	type: "POST",
 	url: url,
-	data: $.param(data),
+	data: data,
 	success: function(){console.log("success!!");}
     });
+   
 });
