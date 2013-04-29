e7eb1383f3a8460ae3c070d1627f11bcac70e084
Ubuntu <ubuntu@ip-10-245-79-26.ec2.internal>
2013-04-29 03:29:16 +0000

    dont know why this wasn't already committed

--- updateArticle.py
+++ updateArticle.py
@@ -9,25 +9,29 @@ def main():
   connection = MongoClient()
   db = connection.raw
   xmlFile = "/home/ubuntu/meteowrite/xmlFiles/"
-  
+  curNum = 0 
+  totalNum = len(listdir(xmlFile))
   for f in listdir(xmlFile):
-      
+    curNum = curNum + 1  
     xmldoc = minidom.parse(xmlFile + f)
     tokenlist = xmldoc.getElementsByTagName('token')
     m = re.match(r".*/(\d+)\.xml", xmlFile + f)
     curId = m.group(1)
     dictList = list()
+    
     for token in tokenlist: 
       word = token.getElementsByTagName('word')[0].childNodes[0].data
       ner = token.getElementsByTagName('NER')[0].childNodes[0].data
       
-      if ner != "DATE" and  ner != "O":
+      if ner != "NUMBER" and ner != "DATE" and  ner != "O":
         newDict = {}
         newDict['text'] = word
         newDict['relevance'] = 0.2
         newDict['ner'] = ner
         dictList.append(newDict)    
     updateIt(curId, dictList,db)
+    if len(dictList) > 3:
+      print curId, " (", curNum, " + ", totalNum, ")"
 
 def updateIt(theId, theList,db):
   found = db.articles.find_one({'id' : int(theId)})
